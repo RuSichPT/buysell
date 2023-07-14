41b22cd3dfece246114f20c25e51b1944c2dc621
pavel.tokarev <tokarevpavel1996@mail.ru>
2023-07-14 16:09:58 +0300

    Добавил сущность Image

---
+++ src/main/java/com/github/rusichpt/buysell/models/Image.java
@@ -0,0 +1,25 @@
+package com.github.rusichpt.buysell.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "images")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String originalFileName;
+    private Long size;
+    private String contentType;
+    private boolean isPreviewImage;
+    @Lob
+    @Column(columnDefinition="BLOB")
+    private byte[] bytes;
+}
